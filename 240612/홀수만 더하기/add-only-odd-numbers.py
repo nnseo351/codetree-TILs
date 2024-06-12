fba5da946be13973589cd20ff0a8e98b96373135
skatj351 <129922753+skatj351@users.noreply.github.com>
2024-06-12 21:55:04 +0900

    Title: 홀수만 더하기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240612/홀수만 더하기/add-only-odd-numbers.py
+++ 240612/홀수만 더하기/add-only-odd-numbers.py
@@ -1,7 +1,11 @@
-n=int(input())
-sum=0
+# 변수 선언, 입력
+n = int(input())
+sum_val = 0
 
-for i in range(n):
-    if n%2==0 and n%3==0:
-        sum += i
-print(sum)
+for _ in range(n):
+    a = int(input())
+    if a % 2 == 1 and a % 3 == 0:
+        sum_val += a
+
+# 출력
+print(sum_val)
