db919e6dc5cc3feac9cbe3f4ae991c929a1acbdb
skatj351 <129922753+skatj351@users.noreply.github.com>
2024-06-12 23:06:22 +0900

    Title: 입력받은 수의 합과 평균, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240612/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
+++ 240612/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
@@ -4,7 +4,7 @@ n = int(input())
 
 for i in range(n):
     z = int(input())
-    if 1 <= z <= 100:
+    if 1 <= z <= 1000:
         sum += z  # 각 정수를 더합니다.
         cnt += 1  # 입력된 정수의 개수를 증가시킵니다.
 
