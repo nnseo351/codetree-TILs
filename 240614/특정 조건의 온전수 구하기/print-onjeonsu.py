147aac196d0356b530120bf9d67b34774cdb4fd4
skatj351 <129922753+skatj351@users.noreply.github.com>
2024-06-14 01:11:17 +0900

    Title: 특정 조건의 온전수 구하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240614/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 240614/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,6 +1,15 @@
-n=int(input())
-for i in range(1, n+1):
-    if i%2==0:
-        continue
+# 입력 받기
+n = int(input())
 
-print(i)
+# 1부터 n까지 반복
+for i in range(1, n + 1):
+    # 조건에 해당하면 continue
+    if i % 2 == 0:
+        continue
+    if i % 10 == 5:
+        continue
+    if i % 3 == 0 and i % 9 != 0:
+        continue
+    
+    # 조건에 해당하지 않으면 출력
+    print(i, end=' ')
